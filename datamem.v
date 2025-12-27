f9e5273c5ebac828dc3dc352489f500df06c1c9e
Pradeep S <pradeepsaravana01@gmail.com>
2025-12-27 14:21:57 +0530

    datamem.v

--- datamem.v
+++ datamem.v
@@ -5,10 +5,13 @@ module datamem (
     input  [7:0] wdata,
     output [7:0] rdata
     );
-    reg [7:0] ram [0:4294967295];
-    always @(posedge clk) begin
-        if (write_en)
-            ram[address] <= wdata;
-    end
-	assign rdata = ram[address];
+	
+	reg [7:0] ram [0:4095]; //4KB Ram
+	always @(posedge clk) begin
+	    if (write_en && !address[31:12]) begin
+		    ram[address[11:0]] <= data_in;
+	    end
+	end
+	assign data_out = ram[address];
+
 endmodule
